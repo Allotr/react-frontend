5e1c5d0e551945f2426774badb05ddd9c1d90301
rafaelpernil2 <mysterysynthman@gmail.com>
2021-08-23 02:32:01 +0200

    feat: Init

---
+++ buildDeploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#npm i
+#npm run build
+docker build -t allotr-react-frontend .
+docker tag allotr-react-frontend rafaelpernil/allotr-react-frontend
+docker push rafaelpernil/allotr-react-frontend
+
+kubectl apply -f ./artifacts/deployment.yaml
+kubectl apply -f ./artifacts/service.yaml
+
+kubectl scale --replicas=0 deployment allotr-react-frontend -n openfaas-fn
+kubectl scale --replicas=2 deployment allotr-react-frontend -n openfaas-fn
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import CounterParent from './components/CounterParent';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hello from React, you have deployed this app successfully! :D</p>
+      <CounterParent></CounterParent>
+    </div>
+  );
+}
+
+export default App;
