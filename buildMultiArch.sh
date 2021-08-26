c73f32c81815500448cde6db60457dce6758c1d9
Rafael Pernil <rafaelpernil@uma.es>
2021-08-26 04:02:28 +0200

    fix: Compilation in Raspberry Pi 4

--- buildMultiArch.sh
+++ buildMultiArch.sh
@@ -3,6 +3,6 @@ pnpm i
 pnpm run build
 
 docker tag allotr-react-frontend rafaelpernil/allotr-react-frontend
-docker buildx build --push --platform linux/arm/v7,linux/amd64 --tag rafaelpernil/allotr-react-frontend .
+docker buildx build --push --platform linux/arm/v7,linux/arm64,linux/amd64  --tag rafaelpernil/allotr-react-frontend .
 #docker push rafaelpernil/allotr-react-frontend
 
