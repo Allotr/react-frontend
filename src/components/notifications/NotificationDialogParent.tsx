f5a9f888793528a913336e00bb625ca206736f5b
Rafael Pernil <rafaelpernil@uma.es>
2021-09-20 16:53:18 +0200

    fix: Too many websocket connections

--- src/components/notifications/NotificationDialogParent.tsx
+++ src/components/notifications/NotificationDialogParent.tsx
@@ -1,20 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { doSubscribe } from "../../utils/subscription-caller";
 import NotificationDialog from "./NotificaitonDialog";
-import { MyNotificationData, MyNotificationDataQuery, MyNotificationDataSub, MyNotificationDataSubSubscription, ResourceNotification } from "allotr-graphql-schema-types"
+import { MyNotificationData, MyNotificationDataQuery, MyNotificationDataSub, MyNotificationDataSubSubscription, ResourceNotification, UserDbObject } from "allotr-graphql-schema-types"
 import { useQuery } from "@apollo/client";
+import { CURRENT_USER_DATA } from "../../consts/global_session_keys";
+import { getSessionValue } from "../../utils/storage-utils";
 
 function NotificationDialogParent() {
     const resourceAvailableNotification = "ResourceAvailableNotification";
     const [myNotifications, setMyNotifications] = useState<ResourceNotification[]>([]);
     const { data, error, loading } = useQuery<MyNotificationDataQuery>(MyNotificationData);
-    doSubscribe<{ data: MyNotificationDataSubSubscription }>(MyNotificationDataSub, newValue => {
-        const filteredNotifications = newValue?.data?.myNotificationDataSub.filter(({ titleRef }) => titleRef === resourceAvailableNotification)
-        if (filteredNotifications.length === 0)
-            return
-        setMyNotifications([]);
-        setMyNotifications(filteredNotifications);
-    });
+    const { _id } = getSessionValue<UserDbObject>(CURRENT_USER_DATA);
+
+    if (_id != null) {
+        doSubscribe<{ data: MyNotificationDataSubSubscription }>(MyNotificationDataSub, newValue => {
+            const filteredNotifications = newValue?.data?.myNotificationDataSub.filter(({ titleRef }) => titleRef === resourceAvailableNotification)
+            if (filteredNotifications.length === 0)
+                return
+            setMyNotifications([]);
+            setMyNotifications(filteredNotifications);
+        });
+    }
+
 
     useEffect(() => {
         if (loading || error) return;
--- src/utils/subscription-caller.ts
+++ src/utils/subscription-caller.ts
@@ -6,19 +6,31 @@ import { CURRENT_USER_DATA } from "../consts/global_session_keys";
 import { getSessionValue } from "./storage-utils";
 import { getLoadedEnvVariables } from "./env-loader";
 
-const doSubscribe = <T>(gqlQuery: DocumentNode, onNext: (newValue: T) => void): void => {
-    const { REACT_APP_WS_API_ENDPOINT } = getLoadedEnvVariables();
-    const { _id } = getSessionValue<UserDbObject>(CURRENT_USER_DATA);
-    const client = createClient({ url: `${REACT_APP_WS_API_ENDPOINT}/subscriptions` });
-    const query = getGqlString(gqlQuery) ?? "";
-    const intervalId = setInterval(() => {
-        if (!_id) return;
-        new Promise<void>((resolve, reject) => {
+let doSubscribe = async <T>(gqlQuery: DocumentNode, onNext: (newValue: T) => void): Promise<void> => {
+    var instance: Promise<void>;
+
+    doSubscribe = async <T>(gqlQuery: DocumentNode, onNext: (newValue: T) => void): Promise<void> => {
+        if (await instance == null) {
+            await createConnection();
+        }
+        return instance;
+    }
+
+    async function createConnection() {
+        const { _id } = getSessionValue<UserDbObject>(CURRENT_USER_DATA);
+        if (await instance != null || _id == null) {
+            return;
+        }
+        const { REACT_APP_WS_API_ENDPOINT } = getLoadedEnvVariables();
+        const client = createClient({ url: `${REACT_APP_WS_API_ENDPOINT}/subscriptions` });
+        const query = getGqlString(gqlQuery) ?? "";
+        instance = new Promise<void>((resolve, reject) => {
             client.subscribe({ query }, { next: onNext, error: reject, complete: resolve });
         });
-        clearInterval(intervalId)
-    }, 1000);
-    return;
+        return instance;
+    }
+
+    return await createConnection();
 }
 
 
