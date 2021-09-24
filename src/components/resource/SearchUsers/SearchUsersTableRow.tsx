678afdbbbbd4e8ce12d39696ee3dea87a6f05b87
Rafael Pernil <rafaelpernil@uma.es>
2021-09-24 02:44:41 +0200

    fix: Search user row text color

--- src/components/resource/SearchUsers/SearchUsersTableRow.tsx
+++ src/components/resource/SearchUsers/SearchUsersTableRow.tsx
@@ -26,7 +26,7 @@ function SearchUsersTableRow({
 
   return (
     <div className="resourceCard bg-purple h-14 w-11/12 ml-3 mt-1 flex justify-between">
-      <p className="text-blue-light align-middle block m-auto ml-2">
+      <p className="text-yellow align-middle block m-auto ml-2">
         {userData.name} {userData.surname} - {userData.username}
       </p>
       <span className="bg-purple-dark w-1 ml-2"></span>
