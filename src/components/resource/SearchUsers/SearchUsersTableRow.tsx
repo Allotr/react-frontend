269e17689abefcc29fc617bdf6e7245baf45ee43
Rafael Pernil <rafaelpernil@uma.es>
2021-09-24 02:54:28 +0200

    fix: Undo style changes

--- src/components/resource/SearchUsers/SearchUsersTableRow.tsx
+++ src/components/resource/SearchUsers/SearchUsersTableRow.tsx
@@ -25,8 +25,8 @@ function SearchUsersTableRow({
   // Button action
 
   return (
-    <div className="resourceCard h-14 w-11/12 ml-3 mt-1 flex justify-between">
-      <p className="text-yellow align-middle block m-auto ml-2">
+    <div className="resourceCard bg-purple h-14 w-11/12 ml-3 mt-1 flex justify-between">
+      <p className="text-blue-light align-middle block m-auto ml-2">
         {userData.name} {userData.surname} - {userData.username}
       </p>
       <span className="bg-purple-dark w-1 ml-2"></span>
