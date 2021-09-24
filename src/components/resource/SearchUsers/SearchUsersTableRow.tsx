fdec9df5207f720104bfc8ea5b26b1528cbbcac8
Rafael Pernil <rafaelpernil@uma.es>
2021-09-24 02:48:00 +0200

    fix: Search user card background color

--- src/components/resource/SearchUsers/SearchUsersTableRow.tsx
+++ src/components/resource/SearchUsers/SearchUsersTableRow.tsx
@@ -25,7 +25,7 @@ function SearchUsersTableRow({
   // Button action
 
   return (
-    <div className="resourceCard bg-purple h-14 w-11/12 ml-3 mt-1 flex justify-between">
+    <div className="resourceCard h-14 w-11/12 ml-3 mt-1 flex justify-between">
       <p className="text-yellow align-middle block m-auto ml-2">
         {userData.name} {userData.surname} - {userData.username}
       </p>
