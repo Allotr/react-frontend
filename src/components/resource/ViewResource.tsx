8152b6e72f7d34e5a9a486f56b5f0869c3cca31f
Rafael Pernil <rafaelpernil@uma.es>
2021-09-24 20:28:31 +0200

    fix: ViewResource initial load

--- src/components/resource/ViewResource.tsx
+++ src/components/resource/ViewResource.tsx
@@ -135,7 +135,7 @@ function ViewResource() {
 
                 </div>
                 <p className="block mt-3   text-yellow ml-3 pl-3 w-4/5">
-                    {viewResource?.name}
+                    {viewResource?.name ?? "\n"}
                 </p>
                 {/* Description */}
                 <label
@@ -146,7 +146,7 @@ function ViewResource() {
                 </label>
                 <p
                     className="block mt-3 pt-1 mb-5  text-yellow ml-3  pl-3 w-4/5 " >
-                    {viewResource?.description ?? ""}</p>
+                    {viewResource?.description ?? "\n"}</p>
 
                 {/* Max Active tickets */}
                 <label
@@ -155,7 +155,7 @@ function ViewResource() {
                 >{`${t("MaxUsersViewResource")}`}</label>
                 <p
                     className="block mt-3   text-yellow ml-3 h-10 pl-3 w-1/4">
-                    {viewResource?.maxActiveTickets}
+                    {viewResource?.maxActiveTickets ?? "\n"}
                 </p>
                 {/* View active users and queue */}
                 <label
