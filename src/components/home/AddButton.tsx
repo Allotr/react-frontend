96200aec820ea9679a9f8e71f96d6953ab3d1d3d
Rafael Pernil <rafaelpernil@uma.es>
2021-09-25 02:29:05 +0200

    fix: Accessibility fixes

--- src/components/home/AddButton.tsx
+++ src/components/home/AddButton.tsx
@@ -18,6 +18,7 @@ function ActionButton({ action, disabled = false }: { action: () => void, disabl
     <button
       className="rounded-full h-16 w-16 md:h-24 md:w-24 flex items-center justify-center fixed bottom-16 right-5 md:bottom-16 md:right-16 bg-purple-light hover:border-blue-light border-4 border-purple-dark"
       onClick={action}
+      aria-label="add"
       disabled={disabled}
       {...{ onMouseEnter, onMouseLeave }}
     >
