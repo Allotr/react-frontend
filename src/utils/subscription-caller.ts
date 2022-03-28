8f0c248aca8e6ff3e9758a43649fac32b4466d01
Rafael Pernil <rafaelpernil@uma.es>
2022-03-28 02:08:00 +0200

    fix: Subscriptions stuck bug

--- src/utils/subscription-caller.ts
+++ src/utils/subscription-caller.ts
@@ -1,13 +1,10 @@
 import { createClient } from "graphql-ws";
 import { getGqlString } from "./data-utils";
 import { DocumentNode } from "@apollo/react-hooks";
-import { UserDbObject } from "allotr-graphql-schema-types";
-import { CURRENT_USER_DATA } from "../consts/global_session_keys";
-import { getSessionValue } from "./storage-utils";
 import { getLoadedEnvVariables } from "./env-loader";
 
 let doSubscribe = async <T>(gqlQuery: DocumentNode, onNext: (newValue: T) => void): Promise<void> => {
-    var instance: Promise<void>;
+    var instance: Promise<void> = Promise.resolve();
 
     doSubscribe = async <T>(gqlQuery: DocumentNode, onNext: (newValue: T) => void): Promise<void> => {
         if (await instance == null) {
@@ -17,10 +14,6 @@ let doSubscribe = async <T>(gqlQuery: DocumentNode, onNext: (newValue: T) => voi
     }
 
     async function createConnection() {
-        const { _id } = getSessionValue<UserDbObject>(CURRENT_USER_DATA);
-        if (await instance != null || _id == null) {
-            return;
-        }
         const { REACT_APP_WS_API_ENDPOINT } = getLoadedEnvVariables();
         const client = createClient({ url: `${REACT_APP_WS_API_ENDPOINT}/subscriptions` });
         const query = getGqlString(gqlQuery) ?? "";
