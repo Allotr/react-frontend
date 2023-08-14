489f6d4ea023c72e5bbe7698367143d0bdb9fdbb
rafaelpernil2 <mysterysynthman@gmail.com>
2023-08-14 20:47:36 +0200

    fix: Module imports in service worker

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,49 @@ export default defineConfig({
     react(),
     VitePWA({
       filename: 'service-worker.ts',
-      injectRegister: 'auto',
+      includeManifestIcons: false,
+      injectRegister: false,
       srcDir: 'src/',
       strategies: 'injectManifest',
+      manifest: {
+        short_name: "Allotr",
+        name: "Allotr",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "64x64 32x32 24x24 16x16",
+            type: "image/x-icon"
+          },
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any"
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any"
+          },
+          {
+            src: "maskable_logo512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "maskable"
+          },
+          {
+            src: "maskable_logo192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "maskable"
+          }
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff"
+      },
       injectManifest: {
         injectionPoint: undefined
       }
