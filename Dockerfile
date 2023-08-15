9b5d518be6b84ae1ff5e7f38481801dd1b3c9e6e
rafaelpernil2 <mysterysynthman@gmail.com>
2023-08-15 17:35:10 +0200

    fix: sw registration & updated Dockerfile tag

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@
 #RUN pnpm run build
 
 
-FROM nginx:1.21.1
+FROM nginx:1.25.1-alpine3.17-slim
 COPY dist/ /usr/share/nginx/html
 COPY nginx.conf /etc/nginx/nginx.conf
 # CMD ["nginx", "-g", "daemon off;"]
--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -8,66 +8,83 @@ type Config = {
 };
 
 export function register(config?: Config) {
-    if ('serviceWorker' in navigator) {
-        // The URL constructor is available in all browsers that support SW.
-        const publicUrl = new URL('./', window.location.href);
-        if (publicUrl.origin !== window.location.origin) {
-            // Our service worker won't work if PUBLIC_URL is on a different origin
-            // from what our page is served on. This might happen if a CDN is used to
-            // serve assets; see https://github.com/facebook/create-react-app/issues/2374
-            return;
-        }
+    if (!('serviceWorker' in navigator)) return;
+    // The URL constructor is available in all browsers that support SW.
+    const publicUrl = new URL('./', window.location.href);
+    if (publicUrl.origin !== window.location.origin && publicUrl.pathname === '/') {
+        // Our service worker won't work if PUBLIC_URL is on a different origin
+        // from what our page is served on. This might happen if a CDN is used to
+        // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+        return;
+    }
 
-        window.addEventListener('load', () => {
-            const swUrl = `./service-worker.js`;
-            registerValidSW(swUrl, config);
-        });
+    window.addEventListener('load', async () => {
+        const swUrl = `./service-worker.js`;
+        await registerValidSW(swUrl, config);
+        await registerWebPush();
+    });
+}
+
+async function registerValidSW(swUrl: string, config?: Config) {
+    try {
+        const registration = await navigator.serviceWorker.register(swUrl, { type: 'module' });
+
+        registration.onupdatefound = () => {
+            const installingWorker = registration.installing;
+            if (installingWorker == null) return;
+
+            installingWorker.onstatechange = () => {
+                if (installingWorker.state !== 'installed') return;
+
+                if (navigator.serviceWorker.controller) {
+                    // At this point, the updated precached content has been fetched,
+                    // but the previous service worker will still serve the older
+                    // content until all client tabs are closed.
+                    console.log(
+                        'New content is available and will be used when all ' +
+                        'tabs for this page are closed. See https://cra.link/PWA.'
+                    );
+
+                    // Execute callback
+                    config?.onUpdate?.(registration);
+                } else {
+                    // At this point, everything has been precached.
+                    // It's the perfect time to display a
+                    // "Content is cached for offline use." message.
+                    console.log('Content is cached for offline use.');
+
+                    // Execute callback
+                    config?.onSuccess?.(registration);
+                }
+            };
+        };
+    } catch (error) {
+        console.error('Error during service worker registration:', error);
     }
 }
 
-function registerValidSW(swUrl: string, config?: Config) {
-    navigator.serviceWorker
-        .register(swUrl, { type: 'module' })
-        .then((registration) => {
-            registration.onupdatefound = () => {
-                const installingWorker = registration.installing;
-                if (installingWorker == null) {
-                    return;
-                }
-                installingWorker.onstatechange = () => {
-                    if (installingWorker.state === 'installed') {
-                        if (navigator.serviceWorker.controller) {
-                            // At this point, the updated precached content has been fetched,
-                            // but the previous service worker will still serve the older
-                            // content until all client tabs are closed.
-                            console.log(
-                                'New content is available and will be used when all ' +
-                                'tabs for this page are closed. See https://cra.link/PWA.'
-                            );
+// // Get existing subscription from service worker, unsubscribe
+// // (`subscription.unsubscribe()`) and unregister it in the server with
+// // a POST request to stop sending push messages to
+// // unexisting endpoint.
+export async function unregister() {
+    if (!('serviceWorker' in navigator)) return;
 
-                            // Execute callback
-                            if (config && config.onUpdate) {
-                                config.onUpdate(registration);
-                            }
-                        } else {
-                            // At this point, everything has been precached.
-                            // It's the perfect time to display a
-                            // "Content is cached for offline use." message.
-                            console.log('Content is cached for offline use.');
+    const registration = await navigator.serviceWorker.ready;
+    const subscription = await registration.pushManager.getSubscription();
 
-                            // Execute callback
-                            if (config && config.onSuccess) {
-                                config.onSuccess(registration);
-                            }
-                        }
-                    }
-                };
-            };
-        })
-        .catch((error) => {
-            console.error('Error during service worker registration:', error);
-        });
+    await subscription?.unsubscribe()
+
+    await fetch(`${VITE_WEBPUSH_ENDPOINT}/webpush/unregister`, {
+        credentials: "include",
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({ subscription })
+    });
 }
+
 
 
 // Get the `registration` from service worker and create a new
@@ -78,77 +95,51 @@ function registerValidSW(swUrl: string, config?: Config) {
 
 const { VITE_WEBPUSH_ENDPOINT } = getLoadedEnvVariables();
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready
-        .then(async function (registration) {
+async function registerWebPush() {
+    if (!('serviceWorker' in navigator)) return;
 
-            // Get the server's public key
-            const response = await fetch(`${VITE_WEBPUSH_ENDPOINT}/webpush/vapidPublicKey`, { credentials: "include" });
-            const vapidPublicKey = await response.text();
-            // Chrome doesn't accept the base64-encoded (string) vapidPublicKey yet
-            // urlBase64ToUint8Array() is defined in /tools.js
-            const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
-            // Subscribe the user
-            return registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: convertedVapidKey
-            });
-        }).then(function (subscription) {
-            return fetch(`${VITE_WEBPUSH_ENDPOINT}/webpush/register`, {
-                credentials: "include",
-                method: 'post',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify({
-                    subscription: subscription
-                })
-            });
-        });
-}
-//   }
+    const registration = await navigator.serviceWorker.ready;
 
-// // Get existing subscription from service worker, unsubscribe
-// // (`subscription.unsubscribe()`) and unregister it in the server with
-// // a POST request to stop sending push messages to
-// // unexisting endpoint.
-export function unregister() {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.ready
-            .then(function (registration) {
-                return registration.pushManager.getSubscription();
-            }).then(function (subscription) {
-                return subscription?.unsubscribe()
-                    .then(function () {
-                        return fetch(`${VITE_WEBPUSH_ENDPOINT}/webpush/unregister`, {
-                            credentials: "include",
-                            method: 'post',
-                            headers: {
-                                'Content-type': 'application/json'
-                            },
-                            body: JSON.stringify({
-                                subscription: subscription
-                            })
-                        });
-                    });
-            });
-    }
+    // Get the server's public key
+    const response = await fetch(`${VITE_WEBPUSH_ENDPOINT}/webpush/vapidPublicKey`, { credentials: "include" });
+    const vapidPublicKey = await response.text();
+    // Chrome doesn't accept the base64-encoded (string) vapidPublicKey yet
+    // _urlBase64ToUint8Array() is defined in /tools.js
+    const convertedVapidKey = _urlBase64ToUint8Array(vapidPublicKey);
+    // Subscribe the user
+    const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: convertedVapidKey
+    });
+
+    await fetch(`${VITE_WEBPUSH_ENDPOINT}/webpush/register`, {
+        credentials: "include",
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            subscription: subscription
+        })
+    });
 }
+
+
 
 
 // This function is needed because Chrome doesn't accept a base64 encoded string
 // as value for applicationServerKey in pushManager.subscribe yet
 // https://bugs.chromium.org/p/chromium/issues/detail?id=802280
-function urlBase64ToUint8Array(base64String: string) {
-    var padding = '='.repeat((4 - base64String.length % 4) % 4);
-    var base64 = (base64String + padding)
+function _urlBase64ToUint8Array(base64String: string) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
         .replace(/-/g, '+')
         .replace(/_/g, '/');
 
-    var rawData = window.atob(base64);
-    var outputArray = new Uint8Array(rawData.length);
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
 
-    for (var i = 0; i < rawData.length; ++i) {
+    for (let i = 0; i < rawData.length; ++i) {
         outputArray[i] = rawData.charCodeAt(i);
     }
     return outputArray;
