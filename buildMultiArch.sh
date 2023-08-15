cf73ae01576a4759363b1753a2fc9f484dd264b2
rafaelpernil2 <mysterysynthman@gmail.com>
2023-08-15 18:43:58 +0200

    fix: Change script buildMultiArch

--- buildMultiArch.sh
+++ buildMultiArch.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-npm i -g pnpm 
-pnpm i
-pnpm run build
-
 docker tag allotr-react-frontend rafaelpernil/allotr-react-frontend
 docker buildx build --push --platform linux/arm/v7,linux/arm64,linux/amd64  --tag rafaelpernil/allotr-react-frontend .
 #docker push rafaelpernil/allotr-react-frontend
