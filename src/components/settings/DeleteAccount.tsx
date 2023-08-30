50c779a4eb6719d7390f7bd0d90f6ff9ce21e775
rafaelpernil2 <mysterysynthman@gmail.com>
2023-08-30 10:44:36 +0200

    fix: updated allotr-graphql-schema-types

--- src/components/settings/DeleteAccount.tsx
+++ src/components/settings/DeleteAccount.tsx
@@ -25,7 +25,7 @@ function DeleteAccount() {
 
     const handleConfirmClick = async () => {
         setDisabled(true);
-        const { data, errors } = await callDeleteUser({ variables: { deleteAllFlag: isFlagChecked, userIdToDelete: _id ?? "" } })
+        const { data, errors } = await callDeleteUser({ variables: { deleteAllFlag: isFlagChecked } })
         setDisabled(false);
         if (errors || data?.deleteUser?.status === OperationResult.Error) {
             return;
